a618fb4178034f46a540ed0520036bcd1b36bdbc
plessbd <plessbd@users.noreply.github.com>
2014-11-06 11:25:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,17 @@ brew install node
 
 brew cask install atom bettertouchtool day-o iterm2 google-chrome spotify the-unarchiver virtualbox vagrant asepsis
 
+echo "installing QuickLook Plugins"
+#https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv betterzipql
+
+echo "Installing APM modules"
+apm install linter linter-jshint linter-javac linter-shellcheck linter-htmlhint file-icons
 
 echo "cleaning up"
 brew cask cleanup
 brew cleanup
+defaults write com.apple.finder QLEnableTextSelection -bool true && killall Finder
 
 echo "restart required for Asepsis"
 sudo shutdown -r now

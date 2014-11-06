24c1adb2cfc9faec426536042593b66352f5ac7c
plessbd <plessbd@users.noreply.github.com>
2014-11-06 16:18:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    # https://github.com/mxcl/homebrew/wiki/installation
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    # http://brew.sh/
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
 fi

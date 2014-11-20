0956e8d67d2f9559012293ca78636506c3a50f9d
plessbd <plessbd@users.noreply.github.com>
2014-11-20 15:43:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,12 @@ brew tap caskroom/versions
 
 brew install node
 
-# instead of day-o (the developer does not like yosemite http://shauninman.com/archive/2011/10/20/day_o_mac_menu_bar_clock) use itsycal (http://www.mowglii.com/itsycal/)
-brew cask install atom bettertouchtool iterm2 itsycal spotify the-unarchiver virtualbox vagrant  asepsis 
-
 brew cask install google-chrome-dev firefoxdeveloperedition
+
+# instead of day-o (the developer does not like yosemite http://shauninman.com/archive/2011/10/20/day_o_mac_menu_bar_clock) use itsycal (http://www.mowglii.com/itsycal/)
+brew cask install adium atom bettertouchtool iterm2 itsycal spotify the-unarchiver virtualbox vagrant  asepsis 
+
+brew cask install java intellij-idea-ce
 
 echo "installing QuickLook Plugins"
 #https://github.com/sindresorhus/quick-look-plugins

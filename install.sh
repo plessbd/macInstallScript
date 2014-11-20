6941dd189c334276dff1b7e3db1e2569093fa6b0
plessbd <plessbd@users.noreply.github.com>
2014-11-20 15:28:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,14 @@ fi
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 brew doctor
 brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
 brew install node
 
 # instead of day-o (the developer does not like yosemite http://shauninman.com/archive/2011/10/20/day_o_mac_menu_bar_clock) use itsycal (http://www.mowglii.com/itsycal/)
-brew cask install atom bettertouchtool iterm2 itsycal google-chrome spotify the-unarchiver virtualbox vagrant  asepsis 
+brew cask install atom bettertouchtool iterm2 itsycal spotify the-unarchiver virtualbox vagrant  asepsis 
+
+brew cask install google-chrome-dev firefoxdeveloperedition
 
 echo "installing QuickLook Plugins"
 #https://github.com/sindresorhus/quick-look-plugins
